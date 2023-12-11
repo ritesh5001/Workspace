fc4738e4c9d18230a79a9f2537eefce3e8e98ef6
Ritesh Kumar Giri <rgiri5001@gmail.com>
2023-12-11 23:51:30 +0530

    data sturcture
    11/12/2023

---
+++ Data_Structure/DSA Babbar/Shorting/insertion_short.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+int arr[]   ;
+for(int i = 1;i<n; i++){
+
+    int temp = arr[i];
+
+    for(int j = i-1;j>0;j++){
+
+        if(arr[j]>temp;)
+
+    }
+
+}
+
+    return 0;
+}
